ade26d02a1a95ca461a2fdeded3dc23212008ed4
takuuo2 <126545872+takuuo2@users.noreply.github.com>
2023-05-01 14:14:21 +0000

    [Fix]opinion>body

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,7 +51,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_044040) do
+ActiveRecord::Schema.define(version: 2023_05_01_140737) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2023_05_01_044040) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "opinion"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
